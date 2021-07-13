5545815c95e22e057b750ecfc0dd75ecb066a482
victorsmorais <victorsanmorais@gmail.com>
2021-07-12 23:22:33 -0300

    iniciado projeto com requerimentos basicos de configuração
    #close1

---
+++ examplepython/github_api.py
@@ -0,0 +1,16 @@
+import requests
+
+
+def buscar_avatar(user):
+    """
+    Busca o avatar de um usuario no Github
+    :param user: string com nome do usuario no github
+    :return: string com url do avatar
+    """
+    url = f'https://api.github.com/users/{user}'
+    resp = requests.get(url)
+    return resp.json()['avatar_url']
+
+
+if __name__ == '__main__':
+    print(buscar_avatar('victorsmorais'))
