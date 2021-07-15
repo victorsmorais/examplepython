825d06f57072d3e38f4800b03dab2c4fc674b0f6
victorsmorais <victorsanmorais@gmail.com>
2021-07-15 18:07:51 -0300

    teste PyPi #4

--- setup.py
+++ setup.py
@@ -128,7 +128,6 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: GNU Affero General Public License v3 ",
         "Operating System :: OS Independent",
-        "Programming Language :: Python",
         "Programming Language :: Python :: 3.9",
         "Framework :: Pytest",
     ],
