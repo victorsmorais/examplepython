a996853638ec48d130afa3b92a74d1945b3d0fa7
victorsmorais <victorsanmorais@gmail.com>
2021-08-04 23:21:08 -0300

    criação spam

---
+++ examplepython/tests/test_spam/test_enviador_de_email.py
@@ -0,0 +1,34 @@
+import pytest
+
+from examplepython.tests.spam.enviador_de_email import Enviador, EmailInvalido
+
+
+def test_criar_enviador_de_email():
+    enviador = Enviador()
+    assert enviador is not None
+
+
+@pytest.mark.parametrize(
+    'remetente',
+    ['victorsanmorais@gmail.com', 'victorsanmorais@icloud.com']
+)
+def test_remetente(remetente):
+    enviador = Enviador()
+    resultado = enviador.enviar(
+        remetente,
+        'victor.morais@graduacao.ie.ufrj.br',
+        'teste do enviador de e-mails',
+        'Se você recebeu esta mensagem, então o enviador está funcionando.'
+    )
+    assert remetente in resultado
+
+
+def test_remetente_invalido(remetente):
+    enviador=Enviador()
+    with pytest.raises(EmailInvalido):
+        enviador.enviar(
+            remetente,
+            'victor.morais@graduacao.ie.ufrj.br',
+            'teste do enviador de e-mails',
+            'Se você recebeu esta mensagem, então o enviador está funcionando.'
+        )
