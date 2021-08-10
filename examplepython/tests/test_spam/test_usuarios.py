563554e0140dfc1bc47f104138a4233878b1c3ca
victorsmorais <victorsanmorais@gmail.com>
2021-08-10 00:53:08 -0300

    testes Usuarios, Sessão e Conexão funcionais

--- examplepython/tests/test_spam/test_usuarios.py
+++ examplepython/tests/test_spam/test_usuarios.py
@@ -1,21 +1,30 @@
+from examplepython.spam.modelos import Conexao, Sessao
+
+
+class Usuario:
+    def __init__(self, nome):
+        self.nome = nome
+        self.id = None
+
+
 def test_salvar_usuario():
     conexao = Conexao()
-    sessao = Sessao()
+    sessao = conexao.gerar_sessao()
     usuario = Usuario(nome='Victor')
     sessao.salvar(usuario)
-    assert isinstance(usuario_id, int)
+    assert isinstance(usuario.id, int)
     sessao.roll_back()
     sessao.fechar()
     conexao.fechar()
 
 
-def test_listarar_usuario():
+def test_listar_usuario():
     conexao = Conexao()
     sessao = Sessao()
     usuarios = [Usuario(nome='Victor'), Usuario(nome='Victor 2')]
     for usuario in usuarios:
         sessao.salvar(usuario)
-    assert usuario == sessao.listar()
+    assert usuarios == sessao.listar()
     sessao.roll_back()
     sessao.fechar()
     conexao.fechar()
